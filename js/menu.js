17ee764a8d60ba9f450d573eb00dd7af9518039b
Timofei Shakhamatov <Timkant7@yandex.ru>
2023-05-17 23:31:26 +0500

    finished menu on 680 screen

--- js/menu.js
+++ js/menu.js
@@ -3,9 +3,10 @@ const burgerButton = document.querySelector('.header__burger-button'),
       navMenu = document.querySelector('.header__nav'),
       headerButton = document.querySelector('.header__button'),
       contactBlock = document.querySelector('.header__contact-block'),
-      container = document.querySelector('.container');
+      container = document.querySelector('.container'),
+      headerContent = document.querySelector('.header__content');
 
-const screenWidth = document.body.clientWidth;
+let screenWidth = document.body.clientWidth;
 
 document.addEventListener('DOMContentLoaded', () => {
     if (screenWidth <= 1023) {
@@ -16,11 +17,41 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+window.addEventListener('resize', () => {
+    screenWidth = document.body.clientWidth;
+    if (screenWidth <= 1023) {
+        navMenu.classList.add('hidden');
+        headerButton.classList.add('hidden');
+        navMenu.classList.add('visually-hidden');
+        headerButton.classList.add('visually-hidden');
+    } else if (navMenu.classList.contains('hidden')) {
+        navMenu.classList.remove('hidden');
+        headerButton.classList.remove('hidden');
+        navMenu.classList.remove('visually-hidden');
+        headerButton.classList.remove('visually-hidden');
+    }
+});
+
 burgerButton.addEventListener('click', () => {
     burgerButton.classList.toggle('active');
-    headerRow.classList.toggle('menu-active');
-    contactBlock.classList.toggle('menu-active');
-    container.classList.toggle('hidden');
+    headerContent.classList.toggle('menu-active');
+    
+    if (headerRow.classList.contains('menu-active')) {
+        headerRow.classList.add('visually-hidden');
+        setTimeout(function() {
+            headerRow.classList.remove('menu-active');
+            headerRow.classList.remove('visually-hidden');
+            container.classList.remove('hidden');
+            contactBlock.classList.remove('menu-active');
+        }, 500);
+    } else {
+        container.classList.add('hidden');
+        contactBlock.classList.add('menu-active');
+        headerRow.classList.remove('visually-hidden');
+        setTimeout(function() {
+            headerRow.classList.add('menu-active');
+        });
+    }
 
     if (navMenu.classList.contains('hidden')) {
         navMenu.classList.remove('hidden');
@@ -28,13 +59,13 @@ burgerButton.addEventListener('click', () => {
         setTimeout(function() {
             navMenu.classList.remove('visually-hidden');
             headerButton.classList.remove('visually-hidden');
-        }, 20);
+        }, 10);
     } else {
         navMenu.classList.add('visually-hidden');
         headerButton.classList.add('visually-hidden');
         setTimeout(function() {
             navMenu.classList.add('hidden');
             headerButton.classList.add('hidden');
-        }, 20);
+        }, 500);
     }
 });
